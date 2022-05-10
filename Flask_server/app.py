5d2531eb4623e2de98799518148ea9d0dd342760
Mechsmith <22887202@student.uwa.edu.au>
2022-05-10 12:33:37 +0800

    Redirect to static html

--- Flask_server/app.py
+++ Flask_server/app.py
@@ -1,10 +1,16 @@
 from flask import Flask
+from flask import render_template, flash, redirect, url_for
 
 app = Flask(__name__)
 
 @app.route("/")
 def init_game():
-    return "<h1>Test Flask</h1> <link aref=chessweeper.html>"
+    return "<h1>Test Flask</h1>"
+
+@app.route("/placeholder")
+def placeholder():
+    return redirect(url_for('Flask-server', filename="placeholder.html"))
 
 if __name__ == "__main__":
     app.run()
+
--- Flask_server/flask_server.py
+++ Flask_server/flask_server.py
@@ -1,10 +1,17 @@
+#This will become one of the flask server files. Currently using app.py to test getting the server running first.
+
 from flask import Flask
+from flask import render_template, flash, redirect, url_for
 
 app = Flask(__name__)
 
 @app.route("/")
 def init_game():
-    return "<h1>Test</h1> <link aref=chessweeper.html>"
+    return "<h1>Test</h1>"
 
 if __name__ == "__main__":
     app.run()
+
+@app.route("/placeholder")
+def placeholder():
+    return redirect(url_for('static', filename="placeholder.html"))
