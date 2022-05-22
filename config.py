f6bd82e697083513053f4169b6f5b6907de3a354
LM3476 <lachjkmcd01@gmail.com>
2022-05-22 15:44:07 +0800

    clean

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-class Config(object):
-
-    SECRET_KEY = os.environ.get('SECRET_KEY', 'not-today-mate')
-
-    #SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'sqlite:///' + os.path.join(basedir, 'app.db'))
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
