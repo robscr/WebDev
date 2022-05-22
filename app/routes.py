731f061b20d9e9b488baf452709c6a10b6955f9f
LM3476 <lachjkmcd01@gmail.com>
2022-05-22 21:50:42 +0800

    Adding popup functionality

--- app/routes.py
+++ app/routes.py
@@ -2,63 +2,6 @@ from flask import render_template, url_for, flash, redirect, session
 from app import app
 
 
-# @app.route('/')
-# @app.route('/index')
-# def index():
-
-#     user = {'username': "Tim"}
-
-<<<<<<< HEAD
-#     some_content = [
-#         {
-#             'creator': {'username': 'John Appleseed'},
-#             'content': 'This is the first piece of content'
-#         },
-
-#         {
-#             'creator': {'username': 'John Doe'},
-#             'content': 'This is another piece of content'
-#         },
-=======
-    for someone in users:
-        properties_list.append((someone.id, someone.username, someone.password_hash))
-    
-    hash_list = []
-
-    for someone in users:
-        hash_list.append(someone.password_hash)
-
-    if form.validate_on_submit():
-
-        if request.method == 'POST':
-            username = request.form.get('username')
-            password = request.form.get('password')
-        
-        for someone in users:
-            try:
-                hashed = User.query.all()[someone.id].password_hash
-                verification = sha256_crypt.verify(password, hashed)
-            except:
-                pass
-            
-            if (username == someone.username) and (verification == True):
-                session['user'] = username
-                global user
-                user = someone
-                return redirect(url_for('index'))
-
-        return "<h1>Wrong username or password - placeholder</h1>"
-        
-    return render_template('login.html', form=form)
->>>>>>> 0d67e4249e2bc631864b13a8ef45619c74fd1404
-
-#         {
-#             'creator': {'username': 'John Citizen'},
-#             'content': 'All this content is written by John(?)'
-#         }
-#     ]
-#     return render_template("index.html", title="Sample Title", user=user, content=some_content)
-
 @app.route('/')
 @app.route('/index')
 @app.route('/chessleship.html')
@@ -97,15 +40,11 @@ def register():
     form = LoginForm()
     
     if form.validate_on_submit():
-<<<<<<< HEAD
-        user = User(username=form.username.data, password_hash=form.password.data)
-=======
         #hashed_password = 
         raw_password = form.password.data
         encrypted_password = sha256_crypt.hash(raw_password)
 
         user = User(username=form.username.data, password_hash=encrypted_password)
->>>>>>> 0d67e4249e2bc631864b13a8ef45619c74fd1404
         db.session.add(user)
         db.session.commit()
         return redirect(url_for('index'))
