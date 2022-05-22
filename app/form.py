37079b7fc488ab45cd12ec75198a5d17fc49ec26
Mechsmith <22887202@student.uwa.edu.au>
2022-05-22 13:29:10 +0800

    Resolve conflicts

--- app/form.py
+++ app/form.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 from app import app
 
 from flask_wtf import FlaskForm
@@ -10,3 +11,17 @@ class RegisterForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
 
     submit = SubmitField('New User')
+=======
+from app import app
+
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BoleanField, SubmitField
+from wtforms.validators import DataRequired
+
+class RegisterForm(FlaskForm):
+
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+
+    submit = SubmitField('New User')
+>>>>>>> 34de3eab90e4205aa9e4b53ee658405404d277f8
