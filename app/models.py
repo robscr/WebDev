2c2f7ecba581a5d15635fd7547b922e1bb04d878
Mechsmith <85858084+Mechsmith@users.noreply.github.com>
2022-05-23 03:20:19 +0800

    Update models.py
    Moved working code into main

--- app/models.py
+++ app/models.py
@@ -4,9 +4,9 @@ class User(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
-    #email = db.Column(db.String(128), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     games_played = db.Column(db.Integer, index=True)
+    average_guesses = db.Column(db.Float, index=True)
 
     def __repr__(self):
         return f'<User {self.username}>'
