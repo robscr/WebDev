05a2afcf22540c9493eac2a79077bc99ba0a9b53
Mechsmith <22887202@student.uwa.edu.au>
2022-05-22 11:08:58 +0800

    Updated requirements.txt - prep for password hashing

--- app/authentication.py
+++ app/authentication.py
@@ -1,12 +1,12 @@
-from app import app
-from flask_login import LoginManager
-from app import db
-from app.models import User
+# from app import app
+# from flask_login import LoginManager
+# from app import db
+# from app.models import User
 
-login_manager = LoginManager()
+# login_manager = LoginManager()
 
-login_manager.init_app(app)
+# login_manager.init_app(app)
 
-@login_manager.user_loader
-def load_user(user_id):
-    return User.get(user_id)
+# @login_manager.user_loader
+# def load_user(user_id):
+#     return User.get(user_id)
