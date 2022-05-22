1a715c97ee3dce19eaa60e05eec16e90499e8922
Mechsmith <22887202@student.uwa.edu.au>
2022-05-22 13:14:42 +0800

    resolving pull conflicts

--- app/form.py
+++ app/form.py
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 from app import app
 
 from flask_wtf import FlaskForm
@@ -12,31 +11,3 @@ class RegisterForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
 
     submit = SubmitField('New User')
-=======
-from app import app
-
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BoleanField, SubmitField
-from wtforms.validators import DataRequired
-
-class RegisterForm(FlaskForm):
-
-    username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
-
-    submit = SubmitField('New User')
->>>>>>> 34de3eab90e4205aa9e4b53ee658405404d277f8
-=======
-from app import app
-
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BoleanField, SubmitField
-from wtforms.validators import DataRequired
-
-class RegisterForm(FlaskForm):
-
-    username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
-
-    submit = SubmitField('New User')
->>>>>>> 34de3eab90e4205aa9e4b53ee658405404d277f8
